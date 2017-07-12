a9643c70154bd52b917e88b89396b04003715ac2
Brett Pongratz <pongo1250@gmail.com>
2017-07-12 16:55:03 -0500

    creted inputStartTime and timeSplit functions

--- Babysitter.py
+++ Babysitter.py
@@ -4,4 +4,14 @@ def inputStartTime(rawStartTime):
 	#check start time is valid
 	#takes a string and seperates it into starrTime int 
 	#and string for am/pm
-	return 0
+	return rawStartTime
+
+
+def timeSplit(rawStartTime):
+	timeInt = rawStartTime[0:-1]
+	timeInt = int(timeInt)
+	DayStr = rawStartTime[-1:]
+
+	li = [timeInt, DayStr]
+
+	return li
--- TestBabysitter.py
+++ TestBabysitter.py
@@ -8,12 +8,26 @@ class TimeValues(unittest.TestCase):
 	#unittest formula is
 	#test_functionName_testDescription
 
-	def test_inputStartTime_testDescription(self):
+	#test function receives a string and retuns an int and a str
+	#
+	def test_inputStartTime_testInput(self):
 		#capture results of function
 		result = Babysitter.inputStartTime("500a")
 		#check for expected output
 		expected = "500a"
 		self.assertEqual(expected, result)
+
+	def test_timeSplit_dayCheck(self):
+		timestr = Babysitter.timeSplit("500a")[1]
+		timeint = Babysitter.timeSplit("500a")[0]
+		expected = "a"
+		expectedInt = 500
+		self.assertEqual(expected, timestr)
+		self.assertEqual(expectedInt, timeint)
+
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
