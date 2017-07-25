c59b5c25910b100a95c45e132d0b8ae87d6a31e8
Brett Pongratz <pongo1250@gmail.com>
2017-07-25 13:20:38 -0500

    changed variable names and added drop down for bed time menu

--- Gui.py
+++ Gui.py
@@ -5,16 +5,17 @@ root = Tk()
 root.title("BabysitterTimeKeeper")
 
 #Dictionaries for Dropdown menu
-sTime = ["5p","6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a" ]
-eTime = ["6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a", "4a" ]
-bTime = ["5p","6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a", "4a"]
+sTList= ["5p","6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a" ]
+eTList = ["6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a", "4a" ]
+bTList = ["5p","6p", "7p", "8p", "9p", "10p", "11p", "12p","1a", "2a", "3a", "4a"]
 
+################# Drop Down Menus#####################
 #Set start time variable to 5p by default
 stimevar = StringVar(root)
 stimevar.set("5p")
 
 #Start time menu 
-stimeMenu = OptionMenu(root, stimevar, *sTime)
+stimeMenu = OptionMenu(root, stimevar, *sTList)
 Label(root, text = "Start Time").grid(row = 1, column = 1)
 stimeMenu.grid(row = 1, column = 2)
 
@@ -23,9 +24,20 @@ etimevar = StringVar(root)
 etimevar.set("6p")
 
 #End time menu
-etimeMenu = OptionMenu(root, etimevar, *eTime)
+etimeMenu = OptionMenu(root, etimevar, *eTList)
 Label(root, text = "End Time").grid(row = 2, column = 1)
 etimeMenu.grid(row =2, column = 2)
+
+#set bed time variable to 5p by default
+btimevar = StringVar(root)
+btimevar.set("5p")
+
+#Bed Time Menu
+btimeMenu = OptionMenu(root, btimevar, *bTList)
+Label(root, text = "Bed Time").grid(row = 3, column = 1)
+btimeMenu.grid(row = 3, column = 2)
+
+
 
 # change drop down menu
 
