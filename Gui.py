8185157e657ff078336a1f8568b1647098570977
Brett Pongratz <pongo1250@gmail.com>
2017-07-25 13:07:33 -0500

    added end time drop down menu

--- Gui.py
+++ Gui.py
@@ -6,15 +6,25 @@ root.title("BabysitterTimeKeeper")
 
 #Dictionaries for Dropdown menu
 sTime = {"5p","6p", "7p", "8p", "9p"}
-eTime = {}
+eTime = {"6p", "7p", "8p"}
 
 #Set start time variable to 5p by default
 stimevar = StringVar(root)
 stimevar.set("5p")
 
-popupMenu = OptionMenu(root, stimevar, *sTime)
+#Start time menu 
+stimeMenu = OptionMenu(root, stimevar, *sTime)
 Label(root, text = "Start Time").grid(row = 1, column = 1)
-popupMenu.grid(row = 1, column = 2)
+stimeMenu.grid(row = 1, column = 2)
+
+#set end time variable to 6p by default
+etimevar = StringVar(root)
+etimevar.set("6p")
+
+#End time menu
+etimeMenu = OptionMenu(root, etimevar, *eTime)
+Label(root, text = "End Time").grid(row = 2, column = 1)
+etimeMenu.grid(row =2, column = 2)
 
 # change drop down menu
 
