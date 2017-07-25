af3e1fbc1d91dab796c02de5aa40a6517dd6873c
Brett Pongratz <pongo1250@gmail.com>
2017-07-25 13:02:54 -0500

    created Start Time drop down menu example

--- Gui.py
+++ Gui.py
@@ -4,7 +4,19 @@ from Tkinter import *
 root = Tk()
 root.title("BabysitterTimeKeeper")
 
+#Dictionaries for Dropdown menu
+sTime = {"5p","6p", "7p", "8p", "9p"}
+eTime = {}
 
+#Set start time variable to 5p by default
+stimevar = StringVar(root)
+stimevar.set("5p")
+
+popupMenu = OptionMenu(root, stimevar, *sTime)
+Label(root, text = "Start Time").grid(row = 1, column = 1)
+popupMenu.grid(row = 1, column = 2)
+
+# change drop down menu
 
 
 
